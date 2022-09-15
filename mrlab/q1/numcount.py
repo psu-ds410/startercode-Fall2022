84defc1798b0853a5c4f1574e2eebb9d77e3f153
Andrew Cavanaugh <97721149+DrewCav@users.noreply.github.com>
2022-09-15 16:54:52 -0400

    Update numcount.py

--- mrlab/q1/numcount.py
+++ mrlab/q1/numcount.py
@@ -3,9 +3,19 @@ from mrjob.job import MRJob   # MRJob version
 # Change the class name!!
 class WordCount(MRJob):  #MRJob version
     def mapper(self, key, line):
+        wordCount=dict()
         words = line.split()
         for w in words:
-            yield (w, 1)
+            length = w.length
+            if length == dict.get(length):
+                dict[length] = dict.get(length)+1
+            else:
+                dict[length] = 1
+        print(dict)
+            #check if length of word exists
+            #if yes add one
+            #else create and set as 1
+            
 
     def reducer(self, key, values):
         yield (key, sum(values))
